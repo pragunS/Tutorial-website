e5ac64c997b39b8e8474cf4044c899cbf0d6cd82
33neeraj <neeraj.23-cse-17@mietjammu.in>
2020-11-02 16:07:06 +0530

    first commit

---
+++ app/config/auth.js
@@ -0,0 +1,11 @@
+
+
+module.exports={
+  
+'googleAuth' :{
+  'clientID':'62552009635-fc43fk4ji8khgks82r77voj4fg9kflni.apps.googleusercontent.com',
+  'clientSecret':'your-key',
+  'callbackURL':'http://localhost:3000/auth/google/callback'  
+}
+  
+};
